81c66259c5a7794b60be010f5ed903a056e3d246
boslqx <bosliangqx@gmail.com>
2025-04-16 23:38:07 +0800

    Initial commit: Flask app setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+
+template_dir = os.path.abspath('templates')
+
+app = Flask(__name__, template_folder=template_dir)
+
+@app.route('/')
+def home():
+    print("Rendering index.html from:", template_dir)
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
