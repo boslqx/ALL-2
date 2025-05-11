61e42e9a5702116be5b06caf2f1f6b5f0a2b7a72
JerickAng <jerickang77@gmail.com>
2025-05-11 22:27:18 +0800

    i not sure how to do gok

--- routes/manager.py
+++ routes/manager.py
@@ -1,7 +1,21 @@
 from flask import Blueprint, render_template, session
+import sqlite3
 
 manager_bp = Blueprint('manager', __name__, template_folder='../templates')
 
 @manager_bp.route('/manager')
 def dashboard():
-    return render_template('manager.html', name=session.get('user_id'))
+    user_id = session.get('user_id')
+
+
+    conn = sqlite3.connect('C:/Users/user/Documents/GitHub/ALL-2/instance/site.db')
+    cursor = conn.cursor()
+
+
+    cursor.execute("SELECT Name FROM User WHERE UserID = ?", (user_id,))
+    result = cursor.fetchone()
+    conn.close()
+
+    name = result[0] if result else "Manager"
+
+    return render_template('manager.html', name=name)
