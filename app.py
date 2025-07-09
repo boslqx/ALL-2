9202cf000b4bc00a3e4b46cac987595ecd2ad04c
boslqx <bosliangqx@gmail.com>
2025-07-09 21:16:50 +0800

    bring back old vode

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from flask_bootstrap import Bootstrap
 from db import db
 from db.models import User, Product, StockAlert, Transaction, TransactionDetails
 from routes.login import login_bp
@@ -14,6 +15,7 @@ template_dir = os.path.abspath('templates')
 
 # app setup
 app = Flask(__name__, template_folder=template_dir)
+Bootstrap(app)
 app.secret_key = 'your_secret_key'
 
 # Database 
@@ -46,7 +48,7 @@ def home():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) 
 
 @app.errorhandler(403)
 def forbidden(e):
